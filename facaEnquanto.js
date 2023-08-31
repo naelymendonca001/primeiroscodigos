87d99d48802abd9a01b9d119dc67c7daa72893fd
naelymendonca001 <naelymendonca60@gmail.com>
2023-08-30 20:49:07 -0400

    primeiroscodigos

--- facaEnquanto.js
+++ facaEnquanto.js
@@ -1,8 +1,6 @@
-var resultado = "";
-var i = 0;
-do {
-  i += 1;
-  resultado += i + " ";
-} while (i < 5);
-alert(resultado);
-  
+var numero =0,soma =0;
+do{
+    numero = parseInt(prompt("numero"));
+    soma += numero;
+} while(numero !=0 );
+alert(soma);
